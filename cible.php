84e98a9616c29259d7937a1e7a92a0ba95944843
Unknown <totalchacal@gmail.com>
2019-05-12 17:01:14 +0200

    fixed minors errors in ciblie.php

--- cible.php
+++ cible.php
@@ -9,7 +9,7 @@ if (isset($_FILES['monfichier']) AND $_FILES['monfichier']['error'] == 0)
 	if (in_array($extension_upload, $extensions_autorisees))
 	{
                         // On peut valider le fichier et le stocker définitivement
-		move_uploaded_file($_FILES['monfichier']['tmp_name'], 'C:\wamp64\www\projectweek\start\uploads/' . basename($_FILES['monfichier']['name']));
+		move_uploaded_file($_FILES['monfichier']['tmp_name'], 'C:\wamp64\www\projectweek\uploads/' . basename($_FILES['monfichier']['name']));
 		echo "L'envoi a bien été effectué !";
 	}
 	$infosfichier = pathinfo($_FILES['monfichier2']['name']);
@@ -18,7 +18,7 @@ if (isset($_FILES['monfichier']) AND $_FILES['monfichier']['error'] == 0)
 	if (in_array($extension_upload, $extensions_autorisees))
 	{
                         // On peut valider le fichier et le stocker définitivement
-		move_uploaded_file($_FILES['monfichier2']['tmp_name'], 'C:\wamp64\www\projectweek\start\uploads/' . basename($_FILES['monfichier2']['name']));
+		move_uploaded_file($_FILES['monfichier2']['tmp_name'], 'C:\wamp64\www\projectweek\uploads/' . basename($_FILES['monfichier2']['name']));
 	}
 	$infosfichier = pathinfo($_FILES['monfichier3']['name']);
     $extension_upload = $infosfichier['extension'];
@@ -26,7 +26,7 @@ if (isset($_FILES['monfichier']) AND $_FILES['monfichier']['error'] == 0)
 	if (in_array($extension_upload, $extensions_autorisees))
 	{
                         // On peut valider le fichier et le stocker définitivement
-		move_uploaded_file($_FILES['monfichier3']['tmp_name'], 'C:\wamp64\www\projectweek\start\uploads/' . basename($_FILES['monfichier3']['name']));
+		move_uploaded_file($_FILES['monfichier3']['tmp_name'], 'C:\wamp64\www\projectweek\uploads/' . basename($_FILES['monfichier3']['name']));
 	}
 }
 ?>
