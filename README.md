045d79ccaa613a608f17d5dfe5b27dc9f994ed95
Unknown <totalchacal@gmail.com>
2019-05-12 17:14:10 +0200

    Added to do in readme and comment in cible.php

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-project_week
+Changer les path pour mettre le dossier de destination des images
+
+TO DO:
+	nommer dynamiquement les dossier de destination
+	gestion d'erreurs
--- cible.php
+++ cible.php
@@ -1,15 +1,11 @@
 <?php
-// Testons si le fichier a bien été envoyé et s'il n'y a pas d'erreur
-if (isset($_FILES['monfichier']) AND $_FILES['monfichier']['error'] == 0)
-{
-                // Testons si l'extension est autorisée
+
     $infosfichier = pathinfo($_FILES['monfichier']['name']);
     $extension_upload = $infosfichier['extension'];
 	$extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
 	if (in_array($extension_upload, $extensions_autorisees))
 	{
-                        // On peut valider le fichier et le stocker définitivement
-		move_uploaded_file($_FILES['monfichier']['tmp_name'], 'C:\wamp64\www\projectweek\uploads/' . basename($_FILES['monfichier']['name']));
+		move_uploaded_file($_FILES['monfichier']['tmp_name'], 'C:\wamp64\www\projectweek\uploads/' . basename($_FILES['monfichier']['name'])); //modifier le path pour choisir ou upload les images
 		echo "L'envoi a bien été effectué !";
 	}
 	$infosfichier = pathinfo($_FILES['monfichier2']['name']);
@@ -17,16 +13,13 @@ if (isset($_FILES['monfichier']) AND $_FILES['monfichier']['error'] == 0)
 	$extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
 	if (in_array($extension_upload, $extensions_autorisees))
 	{
-                        // On peut valider le fichier et le stocker définitivement
-		move_uploaded_file($_FILES['monfichier2']['tmp_name'], 'C:\wamp64\www\projectweek\uploads/' . basename($_FILES['monfichier2']['name']));
+		move_uploaded_file($_FILES['monfichier2']['tmp_name'], 'C:\wamp64\www\projectweek\uploads/' . basename($_FILES['monfichier2']['name'])); //ici aussi
 	}
 	$infosfichier = pathinfo($_FILES['monfichier3']['name']);
     $extension_upload = $infosfichier['extension'];
 	$extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
 	if (in_array($extension_upload, $extensions_autorisees))
 	{
-                        // On peut valider le fichier et le stocker définitivement
-		move_uploaded_file($_FILES['monfichier3']['tmp_name'], 'C:\wamp64\www\projectweek\uploads/' . basename($_FILES['monfichier3']['name']));
+		move_uploaded_file($_FILES['monfichier3']['tmp_name'], 'C:\wamp64\www\projectweek\uploads/' . basename($_FILES['monfichier3']['name'])); //ici aussi
 	}
-}
 ?>
