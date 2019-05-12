6690986b6e43d87b0497e10746ac53cec225912c
Unknown <totalchacal@gmail.com>
2019-05-12 19:13:46 +0200

    added brand option

--- cible.php
+++ cible.php
@@ -1,11 +1,18 @@
 <?php
-
+	$current_file_path = dirname(__FILE__);
+	$fullpath = $current_file_path . DIRECTORY_SEPARATOR . 'uploads/';
+	$marquepath = $fullpath . $_POST['marque']. DIRECTORY_SEPARATOR;
+////Fait le dossier de la marque/////
+	if (is_dir($marquepath) == FALSE){ 
+		mkdir($fullpath.$_POST['marque'] , 0777);
+	}
+///rajoute les fichiers///
     $infosfichier = pathinfo($_FILES['monfichier']['name']);
     $extension_upload = $infosfichier['extension'];
 	$extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
 	if (in_array($extension_upload, $extensions_autorisees))
 	{
-		move_uploaded_file($_FILES['monfichier']['tmp_name'], 'C:\wamp64\www\projectweek\uploads/' . basename($_FILES['monfichier']['name'])); //modifier le path pour choisir ou upload les images
+		move_uploaded_file($_FILES['monfichier']['tmp_name'], $marquepath . basename($_FILES['monfichier']['name']));
 		echo "L'envoi a bien été effectué !";
 	}
 	$infosfichier = pathinfo($_FILES['monfichier2']['name']);
@@ -13,13 +20,13 @@
 	$extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
 	if (in_array($extension_upload, $extensions_autorisees))
 	{
-		move_uploaded_file($_FILES['monfichier2']['tmp_name'], 'C:\wamp64\www\projectweek\uploads/' . basename($_FILES['monfichier2']['name'])); //ici aussi
+		move_uploaded_file($_FILES['monfichier2']['tmp_name'], $marquepath . basename($_FILES['monfichier2']['name']));
 	}
 	$infosfichier = pathinfo($_FILES['monfichier3']['name']);
     $extension_upload = $infosfichier['extension'];
 	$extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
 	if (in_array($extension_upload, $extensions_autorisees))
 	{
-		move_uploaded_file($_FILES['monfichier3']['tmp_name'], 'C:\wamp64\www\projectweek\uploads/' . basename($_FILES['monfichier3']['name'])); //ici aussi
+		move_uploaded_file($_FILES['monfichier3']['tmp_name'], $marquepath . basename($_FILES['monfichier3']['name']));
 	}
 ?>
